ebb39a5457211be17e33b29e654959d98a94b2e9
wenxuanjun <wenxuanjun@yahoo.com>
2024-11-16 07:34:48 +0800

    无限输入缓冲区大小

--- include/pl_readline.h
+++ include/pl_readline.h
@@ -20,7 +20,7 @@
 #define PL_READLINE_KEY_CTRL_C 0x03
 #define PL_READLINE_KEY_BACKSPACE '\b'
 
-#define _SELF pl_readline_t self
+#define _self pl_readline_t self
 #define PL_READLINE_SUCCESS 0
 #define PL_READLINE_FAILED -1
 #define PL_READLINE_NOT_FINISHED 1
@@ -50,40 +50,33 @@ typedef struct pl_readline {
     void (*pl_readline_hal_flush)(void);   // 刷新函数
     void (*pl_readline_get_words)(char *buf,
                                   pl_readline_words_t words); // 获取词组列表
-    list_t history; // 历史记录列表
     char *buffer;         // 输入缓冲区
+    char *input_buf;      // 输入缓冲区（补全的前缀）
+    size_t ptr;           // 输入缓冲区指针
+    size_t input_ptr; // 输入缓冲区（补全的前缀）指针
     size_t maxlen;        // 缓冲区最大长度
-} *pl_readline_t;
-
-typedef struct pl_readline_runtime {
-    char *buffer;         // 输入缓冲区
-    size_t p;             // 输入缓冲区指针
     size_t length;        // 输入缓冲区长度（已经输入的字符数）
+    list_t history;       // 历史记录列表
     int history_idx;      // 历史记录指针
     char *prompt;         // 提示符
-    size_t maxlen;        // 缓冲区最大长度
-    char *input_buf;      // 输入缓冲区（补全的前缀）
-    size_t input_buf_ptr; // 输入缓冲区（补全的前缀）指针
     bool intellisense_mode;  // 智能补全模式
     char *intellisense_word; // 智能补全词组
-} pl_readline_runtime;
+} *pl_readline_t;
 
 pl_readline_words_t pl_readline_word_maker_init(void);
 pl_readline_t pl_readline_init(
     int (*pl_readline_hal_getch)(void), void (*pl_readline_hal_putch)(int ch),
     void (*pl_readline_hal_flush)(void),
     void (*pl_readline_get_words)(char *buf, pl_readline_words_t words));
-const char *pl_readline(_SELF, char *prompt);
-pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
-                                          pl_readline_words_t words);
-void pl_readline_insert_char_and_view(_SELF, char ch, pl_readline_runtime *rt);
+const char *pl_readline(_self, char *prompt);
+pl_readline_word pl_readline_intellisense(_self, pl_readline_words_t words);
+void pl_readline_insert_char_and_view(_self, char ch);
 void pl_readline_insert_char(char *str, char ch, int idx);
 int pl_readline_word_maker_add(char *word, pl_readline_words_t words,
                                bool is_first, char sep);
-void pl_readline_print(_SELF, char *str);
-void pl_readline_intellisense_insert(_SELF, pl_readline_runtime *rt,
-                                     pl_readline_word words);
+void pl_readline_print(_self, char *str);
+void pl_readline_intellisense_insert(_self, pl_readline_word words);
 void pl_readline_word_maker_destroy(pl_readline_words_t words);
-void pl_readline_next_line(_SELF, pl_readline_runtime *rt);
-int pl_readline_handle_key(_SELF, int ch, pl_readline_runtime *rt);
-void pl_readline_uninit(_SELF);
+void pl_readline_next_line(_self);
+int pl_readline_handle_key(_self, int ch);
+void pl_readline_uninit(_self);
--- src/plreadln_intellisense.c
+++ src/plreadln_intellisense.c
@@ -16,15 +16,15 @@
 #include <string.h>
 
 // 向输入缓冲区插入字符
-static void insert_char(_SELF, char ch, pl_readline_runtime *rt) {
-    pl_readline_insert_char_and_view(self, ch, rt);
-    pl_readline_insert_char(rt->input_buf, ch, rt->input_buf_ptr++);
+static void insert_char(_self, char ch) {
+    pl_readline_insert_char_and_view(self, ch);
+    pl_readline_insert_char(self->input_buf, ch, self->input_ptr++);
 }
 
 // 向输入缓冲区插入字符串
-static void insert_string(_SELF, char *str, pl_readline_runtime *rt) {
+static void insert_string(_self, char *str) {
     while (*str) {
-        insert_char(self, *str++, rt);
+        insert_char(self, *str++);
     }
 }
 
@@ -54,10 +54,10 @@ static char *get_same_prefix(pl_readline_words_t words) {
 }
 
 // 检查是否是第一个单词
-static bool check_is_first(pl_readline_runtime *rt) {
-    int p = rt->p;
+static bool check_is_first(_self) {
+    int p = self->ptr;
     while (p) {
-        if (rt->buffer[--p] == ' ') { // 看一下前面有没有空格
+        if (self->buffer[--p] == ' ') { // 看一下前面有没有空格
             return false;             // 不是第一个单词
         }
     }
@@ -65,23 +65,23 @@ static bool check_is_first(pl_readline_runtime *rt) {
 }
 
 // 自动补全
-pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
+pl_readline_word pl_readline_intellisense(_self,
                                           pl_readline_words_t words) {
     char *buf;                          // 用户输入的缓冲区
     int times = 0;                      // 输出补全词库的次数
-    size_t idx;                         // rt->intellisense_word的索引
+    size_t idx;                         // self->intellisense_word的索引
     int flag = 0;                       // 用户输入的词存不存在
-    bool is_first = check_is_first(rt); // 是否是第一个单词
-    if (rt->intellisense_mode ==
+    bool is_first = check_is_first(self); // 是否是第一个单词
+    if (self->intellisense_mode ==
         false) { // 如果是这个模式，则我们需要插入些东西
-        buf = strdup(rt->input_buf); // 保存一下
-        // rt->intellisense_word将会在后面被释放，不用担心内存泄漏
-        rt->intellisense_word = buf;
-        buf[rt->input_buf_ptr] = '\0';         // 加上结束符
-        idx = rt->input_buf_ptr;               // 索引
+        buf = strdup(self->input_buf); // 保存一下
+        // self->intellisense_word将会在后面被释放，不用担心内存泄漏
+        self->intellisense_word = buf;
+        buf[self->input_ptr] = '\0';         // 加上结束符
+        idx = self->input_ptr;               // 索引
     } else {                                   // 列出模式
-        assert(rt->intellisense_word != NULL); // 这个指针一定不为空
-        buf = rt->intellisense_word;           // 重定向buf
+        assert(self->intellisense_word != NULL); // 这个指针一定不为空
+        buf = self->intellisense_word;           // 重定向buf
         idx = strlen(buf);                     // 设置索引
     }
     self->pl_readline_get_words(buf, words); // 请求词库
@@ -91,12 +91,12 @@ pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
         if (strncmp(buf, words->words[i].word, idx) == 0 &&
             (is_first || !words->words[i].first)) { // 找到相同前缀的单词
             if (strlen(words->words[i].word) >
-                rt->input_buf_ptr) // 找到的单词比输入的长
+                self->input_ptr) // 找到的单词比输入的长
                 can_be_selected++; // 可能的单词数加一
             if (strlen(words->words[i].word) ==
-                rt->input_buf_ptr) { // 找到的单词和输入的一样长
-                char *temp_buffer = strdup(rt->input_buf); // 临时缓冲区
-                temp_buffer[rt->input_buf_ptr] = '\0';     // 加上结束符
+                self->input_ptr) { // 找到的单词和输入的一样长
+                char *temp_buffer = strdup(self->input_buf); // 临时缓冲区
+                temp_buffer[self->input_ptr] = '\0';     // 加上结束符
                 if (strcmp(words->words[i].word, temp_buffer) ==
                     0) {                       // 找到的单词和输入一样
                     flag = 1;                  // flag代表有这个词
@@ -112,12 +112,12 @@ pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
     if (can_be_selected == 0 && flag == 1) {
         pl_readline_word_maker_destroy(words); // 释放words
         if (sep) // 有分隔符，插入一下即可
-            pl_readline_handle_key(self, sep, rt);
+            pl_readline_handle_key(self, sep);
         return (pl_readline_word){0};
     }
 
     pl_readline_words_t words_temp = NULL;
-    if (!rt->intellisense_mode) {
+    if (!self->intellisense_mode) {
         words_temp =
             pl_readline_word_maker_init(); // 临时词库，用于存储“可能”的单词
         if (idx == 0) {                    // 没东西我们直接输出词库
@@ -128,17 +128,17 @@ pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
         if (strncmp(buf, words->words[i].word, idx) == 0 &&
             (is_first || !words->words[i].first)) {
             // 和用户输入一样的单词有什么好必要补全的？
-            if (!rt->intellisense_mode && strlen(words->words[i].word) == idx) {
+            if (!self->intellisense_mode && strlen(words->words[i].word) == idx) {
                 // 直接下一个
                 continue;
             }
-            if (!rt->intellisense_mode) { // 如果是需要输出模式
+            if (!self->intellisense_mode) { // 如果是需要输出模式
                 pl_readline_word_maker_add(words->words[i].word, words_temp,
                                            false,
                                            0); // 加入临时词库
             } else {
                 if (times == 0) {                    // 第一次输出
-                    pl_readline_next_line(self, rt); // 换行
+                    pl_readline_next_line(self); // 换行
                 }
                 if (times)                        // 不是第一次输出
                     pl_readline_print(self, " "); // 空格分隔
@@ -148,7 +148,7 @@ pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
         }
     }
     pl_readline_word ret = {0};
-    if (rt->intellisense_mode == false) {
+    if (self->intellisense_mode == false) {
         // 找到相同前缀的单词
         // 作者按：这里 get_same_prefix 不可能找不到相同前缀的单词，
         // 因为 words_temp
@@ -162,27 +162,27 @@ pl_readline_word pl_readline_intellisense(_SELF, pl_readline_runtime *rt,
         if (prefix && strcmp(prefix, buf) == 0) {
             free(prefix);
         NOTHING_INPUT:
-            rt->intellisense_mode = true;               // 设置为列出模式
+            self->intellisense_mode = true;               // 设置为列出模式
             pl_readline_word_maker_destroy(words_temp); // 释放words_temp
             pl_readline_word_maker_destroy(words);      // 释放words
             words = pl_readline_word_maker_init();      // 重新初始化words
 
-            return pl_readline_intellisense(self, rt, words);
+            return pl_readline_intellisense(self, words);
         }
         // 若prefix为NULL，则不会去插入，这是调用者决定的，我这里不管
         ret.word = prefix;
-        rt->intellisense_mode = true;
+        self->intellisense_mode = true;
         pl_readline_word_maker_destroy(words_temp);
     }
-    if (rt->intellisense_mode && times) {
+    if (self->intellisense_mode && times) {
         ret.first = true;
     }
     pl_readline_word_maker_destroy(words); // 释放words
     return ret;
 }
 
-void pl_readline_intellisense_insert(_SELF, pl_readline_runtime *rt,
+void pl_readline_intellisense_insert(_self,
                                      pl_readline_word word) {
-    insert_string(self, word.word + rt->input_buf_ptr, rt);
+    insert_string(self, word.word + self->input_ptr);
     free(word.word);
 }
